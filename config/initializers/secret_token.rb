89403789477c32baf30ba488ce7bf68e4ad8bb50
Derick Hitchcock <derick@email.unc.edu>
2011-07-08 19:09:08 -0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Metrics::Application.config.secret_token = 'b071c79f812b3e5ed58dedb5f11b18fe79c3ec39bb7b457e58737c0091894372eb0273ae4fdd29b12d3cf51fa165c411ca3ec507a87df97c77e44542cfc00b40'
