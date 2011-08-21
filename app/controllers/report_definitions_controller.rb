ca1f72af6652560b8c44415ba5aea9723b753ecf
dustin <dstywho@gmail.com>
2011-08-21 19:52:13 -0400

    report definition done

--- app/controllers/report_definitions_controller.rb
+++ app/controllers/report_definitions_controller.rb
@@ -25,10 +25,8 @@ class ReportDefinitionsController < ApplicationController
   # GET /report_definitions/new.xml
   def new
     @report_definition = ReportDefinition.new
-    @projects = Project.all
     @report_definition.project_report_definitions.build
-    @report_definition.projects.build
-    @report_definition.projects.build
+    @report_definition.evaluations.build 
 
     respond_to do |format|
       format.html # new.html.erb
