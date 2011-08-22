2081a85ac1657ef445e5cfa6201a578846bf76b5
dustin <dstywho@gmail.com>
2011-08-22 09:43:29 -0400

    evaluations belongs to updated

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -1,5 +1,6 @@
 class Evaluation < ActiveRecord::Base
-  belongs_to :gradeing_scale
+  belongs_to :grading_scale
   belongs_to :formula
   belongs_to :report_definition
+  accepts_nested_attributes_for :grading_scale,:formula, :allow_destroy => true
 end
